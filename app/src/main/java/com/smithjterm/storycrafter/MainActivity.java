b82aa44985a948e3db627f165fec7c98cdf9dada
Demo User <demouser@MacBook-Air.local>
2018-01-12 14:39:15 -0500

    Practice commit.

--- app/src/main/java/com/smithjterm/storycrafter/MainActivity.java
+++ app/src/main/java/com/smithjterm/storycrafter/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // let's tell some stories! :D - sarah
+    // let's tell some sci-fi stories! :D - sarah
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
