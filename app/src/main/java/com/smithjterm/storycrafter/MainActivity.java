546a3437a0e38d5050145999f09895d34c561680
Demo User <demouser@MacBook-Air.local>
2018-01-16 11:52:12 -0500

    Implemented StoryTree.

--- app/src/main/java/com/smithjterm/storycrafter/MainActivity.java
+++ app/src/main/java/com/smithjterm/storycrafter/MainActivity.java
@@ -2,6 +2,7 @@ package com.smithjterm.storycrafter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StoryTree mainTree = new StoryTree();
+
+        mainTree.setLeft(new StoryTree());
+        mainTree.setRight(new StoryTree());
+
+        mainTree.getLeft().setLeft(new StoryTree());
+        mainTree.getLeft().setRight(new StoryTree());
+
+        mainTree.getRight().setLeft(new StoryTree());
+        mainTree.getRight().setRight(new StoryTree());
+
+        // The test below makes sure isFull works.
+
+        //  String result = "test result: "+mainTree.isFull();
+        //  Log.i("MainActivity",result);
     }
 }
---
+++ app/src/main/java/com/smithjterm/storycrafter/StoryTree.java
@@ -0,0 +1,89 @@
+package com.smithjterm.storycrafter;
+
+/**
+ * Created by Sarah Abowitz on 1/16/18.
+ */
+
+public class StoryTree {
+    private StoryTree left;
+    private StoryTree right;
+
+    private String title;
+    private String body;
+    private String choice1txt;
+    private String choice2txt;
+
+    public StoryTree(){
+      title = "";
+      body = "";
+      choice1txt = "";
+      choice2txt = "";
+    }
+
+    public StoryTree(String t, String b, String c1, String c2){
+      title = t;
+      body = b;
+      choice1txt = c1;
+      choice2txt = c2;
+    }
+
+    public StoryTree getLeft() {
+        return left;
+    }
+
+    public StoryTree getRight() {
+        return right;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getChoice1txt() {
+        return choice1txt;
+    }
+
+    public String getChoice2txt() {
+        return choice2txt;
+    }
+
+    public void setLeft(StoryTree left) {
+        this.left = left;
+    }
+
+    public void setRight(StoryTree right) {
+        this.right = right;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setChoice1txt(String choice1txt) {
+        this.choice1txt = choice1txt;
+    }
+
+    public void setChoice2txt(String choice2txt) {
+        this.choice2txt = choice2txt;
+    }
+
+    public boolean isFull(){
+      if (title.length() < 1 || body.length() < 1 || choice1txt.length() < 1 || choice2txt.length() < 1){
+          return false;
+      }
+
+      if (left != null && right != null){
+          return (left.isFull() && right.isFull());
+      }
+
+      return true;
+    }
+}
