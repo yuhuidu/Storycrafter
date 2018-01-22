a544f671b31aea16f882ed6ccbdbb9d7ba61f6bb
Demo User <demouser@MacBook-Air.local>
2018-01-21 20:59:24 -0500

    more buttons worked

--- app/src/main/java/com/smithjterm/storycrafter/MainActivity.java
+++ app/src/main/java/com/smithjterm/storycrafter/MainActivity.java
@@ -27,17 +27,18 @@ public class MainActivity extends AppCompatActivity {
     public static final String EDIT_CHOICE_1_KEY = "edit choice 1";
     public static final String EDIT_CHOICE_2_KEY = "edit choice 2";
 
-
     // key for nodes below, this has nothing to do with the id field
     //     1
     //   2   3
     //  4 5 6 7
 
     public static final String NODE_1_INFO_KEY = "node 1 title";
-
     public static final String NODE_2_INFO_KEY = "node 2 title";
-
     public static final String NODE_3_INFO_KEY = "node 3 title";
+    public static final String NODE_4_INFO_KEY = "node 4 title";
+    public static final String NODE_5_INFO_KEY = "node 5 title";
+    public static final String NODE_6_INFO_KEY = "node 6 title";
+    public static final String NODE_7_INFO_KEY = "node 7 title";
 
     public static final String NODE_4_TITLE_KEY = "node 4 title";
     public static final String NODE_4_BODY_KEY = "node 4 body";
@@ -51,8 +52,33 @@ public class MainActivity extends AppCompatActivity {
     public static final String NODE_7_TITLE_KEY = "node 7 title";
     public static final String NODE_7_BODY_KEY = "node 7 body";
 
+    public static final String NODE_8_TITLE_KEY = "node 8 title";
+    public static final String NODE_8_BODY_KEY = "node 8 body";
+
+    public static final String NODE_9_TITLE_KEY = "node 9 title";
+    public static final String NODE_9_BODY_KEY = "node 9 body";
+
+    public static final String NODE_10_TITLE_KEY = "node 10 title";
+    public static final String NODE_10_BODY_KEY = "node 10 body";
+
+    public static final String NODE_11_TITLE_KEY = "node 11 title";
+    public static final String NODE_11_BODY_KEY = "node 11 body";
+
+    public static final String NODE_12_TITLE_KEY = "node 12 title";
+    public static final String NODE_12_BODY_KEY = "node 12 body";
+
+    public static final String NODE_13_TITLE_KEY = "node 13 title";
+    public static final String NODE_13_BODY_KEY = "node 13 body";
+
+    public static final String NODE_14_TITLE_KEY = "node 14 title";
+    public static final String NODE_14_BODY_KEY = "node 14 body";
+
+    public static final String NODE_15_TITLE_KEY = "node 15 title";
+    public static final String NODE_15_BODY_KEY = "node 15 body";
+
     StoryTree mainTree = new StoryTree(0);
     private ViewGroup rootView;
+    public static boolean addButtonClicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -129,18 +155,27 @@ public class MainActivity extends AppCompatActivity {
                 tweakNode.setChoice1txt(extras.getString(EDIT_CHOICE_1_KEY));
                 tweakNode.setChoice2txt(extras.getString(EDIT_CHOICE_2_KEY));
             }
-        } else if (requestCode == RESTART_REQUEST){
+        }
+        else if (requestCode == RESTART_REQUEST){
             if (resultCode == RESULT_OK){
                 mainTree = new StoryTree(0);
                 mainTree.setLeft(new StoryTree(2));
                 mainTree.setRight(new StoryTree(1));
+                mainTree.getLeft().setLeft(new StoryTree( 4));
+                mainTree.getLeft().setRight(new StoryTree( 6));
+                mainTree.getRight().setLeft(new StoryTree(5));
+                mainTree.getRight().setRight(new StoryTree(3));
+            }
+            if (addButtonClicked){
+                mainTree.getLeft().getLeft().setLeft(new StoryTree("", "", "This is an ending", "Choices will not show", 8));
+                mainTree.getLeft().getLeft().setRight(new StoryTree("", "", "This is an ending", "Choices will not show", 9));
+                mainTree.getLeft().getRight().setLeft(new StoryTree("", "", "This is an ending", "Choices will not show", 10));
+                mainTree.getLeft().getRight().setRight(new StoryTree("", "", "This is an ending", "Choices will not show", 11));
 
-                mainTree.getLeft().setLeft(new StoryTree("", "", "This is an ending", "Choices will not show",4));
-                mainTree.getLeft().setRight(new StoryTree("", "", "This is an ending", "Choices will not show",6));
-
-                mainTree.getRight().setLeft(new StoryTree("", "", "This is an ending", "Choices will not show",5));
-                mainTree.getRight().setRight(new StoryTree("", "", "This is an ending", "Choices will not show",3));
-
+                mainTree.getRight().getLeft().setLeft(new StoryTree("", "", "This is an ending", "Choices will not show", 12));
+                mainTree.getRight().getLeft().setRight(new StoryTree("", "", "This is an ending", "Choices will not show", 13));
+                mainTree.getRight().getRight().setLeft(new StoryTree("", "", "This is an ending", "Choices will not show", 14));
+                mainTree.getRight().getRight().setRight(new StoryTree("", "", "This is an ending", "Choices will not show", 15));
             }
         }
     }
@@ -150,26 +185,64 @@ public class MainActivity extends AppCompatActivity {
         //Log.i("MainActivity",mainTree.toString(""));
 
         if (mainTree.isFull()) {
+            if (!addButtonClicked) {
+                Intent intent = new Intent(this, PlayActivity.class);
 
-            Intent intent = new Intent(this, PlayActivity.class);
+                intent.putExtra(NODE_1_INFO_KEY, mainTree.packageAssets());
+                intent.putExtra(NODE_2_INFO_KEY, mainTree.getLeft().packageAssets());
+                intent.putExtra(NODE_3_INFO_KEY, mainTree.getRight().packageAssets());
 
-            intent.putExtra(NODE_1_INFO_KEY, mainTree.packageAssets());
-            intent.putExtra(NODE_2_INFO_KEY, mainTree.getLeft().packageAssets());
-            intent.putExtra(NODE_3_INFO_KEY, mainTree.getRight().packageAssets());
+                intent.putExtra(NODE_4_TITLE_KEY, mainTree.getLeft().getLeft().getTitle());
+                intent.putExtra(NODE_4_BODY_KEY, mainTree.getLeft().getLeft().getBody());
 
-            intent.putExtra(NODE_4_TITLE_KEY, mainTree.getLeft().getLeft().getTitle());
-            intent.putExtra(NODE_4_BODY_KEY, mainTree.getLeft().getLeft().getBody());
+                intent.putExtra(NODE_5_TITLE_KEY, mainTree.getLeft().getRight().getTitle());
+                intent.putExtra(NODE_5_BODY_KEY, mainTree.getLeft().getRight().getBody());
 
-            intent.putExtra(NODE_5_TITLE_KEY, mainTree.getLeft().getRight().getTitle());
-            intent.putExtra(NODE_5_BODY_KEY, mainTree.getLeft().getRight().getBody());
+                intent.putExtra(NODE_6_TITLE_KEY, mainTree.getRight().getLeft().getTitle());
+                intent.putExtra(NODE_6_BODY_KEY, mainTree.getRight().getLeft().getBody());
 
-            intent.putExtra(NODE_6_TITLE_KEY, mainTree.getRight().getLeft().getTitle());
-            intent.putExtra(NODE_6_BODY_KEY, mainTree.getRight().getLeft().getBody());
+                intent.putExtra(NODE_7_TITLE_KEY, mainTree.getRight().getRight().getTitle());
+                intent.putExtra(NODE_7_BODY_KEY, mainTree.getRight().getRight().getBody());
 
-            intent.putExtra(NODE_7_TITLE_KEY, mainTree.getRight().getRight().getTitle());
-            intent.putExtra(NODE_7_BODY_KEY, mainTree.getRight().getRight().getBody());
+                startActivityForResult(intent, RESTART_REQUEST);
+            }
+            else{
+                Intent intent = new Intent(this, PlayActivity.class);
 
-            startActivityForResult(intent, RESTART_REQUEST);
+                intent.putExtra(NODE_1_INFO_KEY, mainTree.packageAssets());
+                intent.putExtra(NODE_2_INFO_KEY, mainTree.getLeft().packageAssets());
+                intent.putExtra(NODE_3_INFO_KEY, mainTree.getRight().packageAssets());
+                intent.putExtra(NODE_4_INFO_KEY, mainTree.getLeft().getLeft().packageAssets());
+                intent.putExtra(NODE_5_INFO_KEY, mainTree.getLeft().getRight().packageAssets());
+                intent.putExtra(NODE_6_INFO_KEY, mainTree.getRight().getLeft().packageAssets());
+                intent.putExtra(NODE_7_INFO_KEY, mainTree.getRight().getRight().packageAssets());
+
+                intent.putExtra(NODE_8_TITLE_KEY, mainTree.getLeft().getLeft().getLeft().getTitle());
+                intent.putExtra(NODE_8_BODY_KEY, mainTree.getLeft().getLeft().getLeft().getBody());
+
+                intent.putExtra(NODE_9_TITLE_KEY, mainTree.getLeft().getLeft().getRight().getTitle());
+                intent.putExtra(NODE_9_BODY_KEY, mainTree.getLeft().getLeft().getRight().getBody());
+
+                intent.putExtra(NODE_10_TITLE_KEY, mainTree.getLeft().getRight().getLeft().getTitle());
+                intent.putExtra(NODE_10_BODY_KEY, mainTree.getLeft().getRight().getLeft().getBody());
+
+                intent.putExtra(NODE_11_TITLE_KEY, mainTree.getLeft().getRight().getRight().getTitle());
+                intent.putExtra(NODE_11_BODY_KEY, mainTree.getLeft().getRight().getRight().getBody());
+
+                intent.putExtra(NODE_12_TITLE_KEY, mainTree.getRight().getLeft().getLeft().getTitle());
+                intent.putExtra(NODE_12_BODY_KEY, mainTree.getRight().getLeft().getLeft().getBody());
+
+                intent.putExtra(NODE_13_TITLE_KEY, mainTree.getRight().getLeft().getRight().getTitle());
+                intent.putExtra(NODE_13_BODY_KEY, mainTree.getRight().getLeft().getRight().getBody());
+
+                intent.putExtra(NODE_14_TITLE_KEY, mainTree.getRight().getRight().getLeft().getTitle());
+                intent.putExtra(NODE_14_BODY_KEY, mainTree.getRight().getRight().getLeft().getBody());
+
+                intent.putExtra(NODE_15_TITLE_KEY, mainTree.getRight().getRight().getRight().getTitle());
+                intent.putExtra(NODE_15_BODY_KEY, mainTree.getRight().getRight().getRight().getBody());
+
+                startActivityForResult(intent, RESTART_REQUEST);
+            }
         }
         else{
             Toast.makeText(MainActivity.this,
@@ -183,27 +256,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addActivity(View view){
-        ImageButton button = new ImageButton(MainActivity.this);
-        button.setImageResource(R.drawable.button1);
-        button.setId(2131165261+1);
-        ImageButton button1 = (ImageButton) findViewById(R.id.imageButton1);
-        mainTree.getLeft().getLeft().setLeft(new StoryTree(7));
+        if (!addButtonClicked) {
+            for (int i = 1; i < 9; i++) {
+                ImageButton button = new ImageButton(MainActivity.this);
+                //button.setImageResource(R.drawable.button1);
+                button.setId(2131165261 + i);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startNewActivity(v);
+                setTreeInAddActivity(i);
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startNewActivity(v);
+                    }
+                });
+
+                Log.i("new node id", "" + button.getId());
+
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                button.setBackgroundResource(R.drawable.button2);
+                button.setPadding(55, 55, 55, 55);
+                params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+                rootView.addView(button, params);
             }
-        });
+            addButtonClicked = true;
+        }
+    }
 
-        Log.i("new node id", ""+button.getId());
-        Log.i("first node id", ""+button1.getId());
-
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-
-        params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
-        rootView.addView(button, params);
+    public void setTreeInAddActivity(int i){
+        if (i == 1){
+            mainTree.getLeft().getLeft().setLeft(new StoryTree(6+i));
+        }
+        else if (i == 2){
+            mainTree.getLeft().getLeft().setRight(new StoryTree(6+i));
+        }
+        else if (i == 3){
+            mainTree.getLeft().getRight().setLeft(new StoryTree(6+i));
+        }
+        else if (i == 4){
+            mainTree.getLeft().getRight().setRight(new StoryTree(6+i));
+        }
+        else if (i == 5){
+            mainTree.getRight().getLeft().setLeft(new StoryTree(6+i));
+        }
+        else if (i == 6){
+            mainTree.getRight().getLeft().setRight(new StoryTree(6+i));
+        }
+        else if (i == 7){
+            mainTree.getRight().getRight().setLeft(new StoryTree(6+i));
+        }
+        else if (i == 8){
+            mainTree.getRight().getRight().setRight(new StoryTree(6+i));
+        }
     }
 
 }
--- app/src/main/java/com/smithjterm/storycrafter/PlayActivity.java
+++ app/src/main/java/com/smithjterm/storycrafter/PlayActivity.java
@@ -29,43 +29,97 @@ public class PlayActivity extends AppCompatActivity {
 
         Bundle extras = i.getExtras();
 
-        StoryTree[] storyTrees = new StoryTree[7];
+        if (MainActivity.addButtonClicked == false) {
+            StoryTree[] storyTrees = new StoryTree[7];
 
-        storyTrees[0] = makeTree(1, extras.getStringArray(MainActivity.NODE_1_INFO_KEY));
-        storyTrees[1] = makeTree(2, extras.getStringArray(MainActivity.NODE_2_INFO_KEY));
-        storyTrees[2] = makeTree(3, extras.getStringArray(MainActivity.NODE_3_INFO_KEY));
+            storyTrees[0] = makeTree(1, extras.getStringArray(MainActivity.NODE_1_INFO_KEY));
+            storyTrees[1] = makeTree(2, extras.getStringArray(MainActivity.NODE_2_INFO_KEY));
+            storyTrees[2] = makeTree(3, extras.getStringArray(MainActivity.NODE_3_INFO_KEY));
 
-        storyTrees[3] = makeEnd(4, extras.getString(MainActivity.NODE_4_TITLE_KEY),
-        extras.getString(MainActivity.NODE_4_BODY_KEY));
+            storyTrees[3] = makeEnd(4, extras.getString(MainActivity.NODE_4_TITLE_KEY),
+                    extras.getString(MainActivity.NODE_4_BODY_KEY));
 
-        storyTrees[4] = makeEnd(5, extras.getString(MainActivity.NODE_5_TITLE_KEY),
-                extras.getString(MainActivity.NODE_5_BODY_KEY));
+            storyTrees[4] = makeEnd(5, extras.getString(MainActivity.NODE_5_TITLE_KEY),
+                    extras.getString(MainActivity.NODE_5_BODY_KEY));
 
-        storyTrees[5] = makeEnd(6, extras.getString(MainActivity.NODE_6_TITLE_KEY),
-                extras.getString(MainActivity.NODE_6_BODY_KEY));
+            storyTrees[5] = makeEnd(6, extras.getString(MainActivity.NODE_6_TITLE_KEY),
+                    extras.getString(MainActivity.NODE_6_BODY_KEY));
 
-        storyTrees[6] = makeEnd(7, extras.getString(MainActivity.NODE_7_TITLE_KEY),
-                extras.getString(MainActivity.NODE_7_BODY_KEY));
+            storyTrees[6] = makeEnd(7, extras.getString(MainActivity.NODE_7_TITLE_KEY),
+                    extras.getString(MainActivity.NODE_7_BODY_KEY));
 
-        storyTrees[0].setChildren(storyTrees[1],storyTrees[2]);
-        storyTrees[1].setChildren(storyTrees[3],storyTrees[4]);
-        storyTrees[2].setChildren(storyTrees[5],storyTrees[6]);
+            storyTrees[0].setChildren(storyTrees[1], storyTrees[2]);
+            storyTrees[1].setChildren(storyTrees[3], storyTrees[4]);
+            storyTrees[2].setChildren(storyTrees[5], storyTrees[6]);
 
-        // Log.i("PlayActivity",storyTrees[0].toString(""));
+
+            // Log.i("PlayActivity",storyTrees[0].toString(""));
 
         /* for (StoryTree s : storyTrees){
             Log.i("PlayActivity",s.toString(""));
         }*/
 
-        currentTree = storyTrees[0];
-        homeTree = storyTrees[0];
+            currentTree = storyTrees[0];
+            homeTree = storyTrees[0];
 
-        String choiceMsg = currentTree.getBody()+"\n>"+currentTree.getChoice1Txt()+"\n>"+currentTree.getChoice2Txt();
-        Log.i("PlayActivity",choiceMsg);
-        title.setText(currentTree.getTitle());
-        body.setText(currentTree.getBody());
-        c1.setText(currentTree.getChoice1Txt());
-        c2.setText(currentTree.getChoice2Txt());
+            String choiceMsg = currentTree.getBody() + "\n>" + currentTree.getChoice1Txt() + "\n>" + currentTree.getChoice2Txt();
+            Log.i("PlayActivity", choiceMsg);
+            title.setText(currentTree.getTitle());
+            body.setText(currentTree.getBody());
+            c1.setText(currentTree.getChoice1Txt());
+            c2.setText(currentTree.getChoice2Txt());
+        }
+        else{
+            StoryTree[] storyTrees = new StoryTree[15];
+            storyTrees[0] = makeTree(1, extras.getStringArray(MainActivity.NODE_1_INFO_KEY));
+            storyTrees[1] = makeTree(2, extras.getStringArray(MainActivity.NODE_2_INFO_KEY));
+            storyTrees[2] = makeTree(3, extras.getStringArray(MainActivity.NODE_3_INFO_KEY));
+            storyTrees[3] = makeTree(4, extras.getStringArray(MainActivity.NODE_4_TITLE_KEY));
+            storyTrees[4] = makeTree(5, extras.getStringArray(MainActivity.NODE_5_TITLE_KEY));
+            storyTrees[5] = makeTree(6, extras.getStringArray(MainActivity.NODE_6_TITLE_KEY));
+            storyTrees[6] = makeTree(7, extras.getStringArray(MainActivity.NODE_7_TITLE_KEY));
+
+            storyTrees[7] = makeEnd(8, extras.getString(MainActivity.NODE_8_TITLE_KEY),
+                    extras.getString(MainActivity.NODE_8_BODY_KEY));
+
+            storyTrees[8] = makeEnd(9, extras.getString(MainActivity.NODE_9_TITLE_KEY),
+                    extras.getString(MainActivity.NODE_9_BODY_KEY));
+
+            storyTrees[9] = makeEnd(10, extras.getString(MainActivity.NODE_10_TITLE_KEY),
+                    extras.getString(MainActivity.NODE_10_BODY_KEY));
+
+            storyTrees[10] = makeEnd(11, extras.getString(MainActivity.NODE_11_TITLE_KEY),
+                    extras.getString(MainActivity.NODE_11_BODY_KEY));
+            storyTrees[11] = makeEnd(12, extras.getString(MainActivity.NODE_12_TITLE_KEY),
+                    extras.getString(MainActivity.NODE_12_BODY_KEY));
+
+            storyTrees[12] = makeEnd(13, extras.getString(MainActivity.NODE_13_TITLE_KEY),
+                    extras.getString(MainActivity.NODE_13_BODY_KEY));
+
+            storyTrees[13] = makeEnd(14, extras.getString(MainActivity.NODE_14_TITLE_KEY),
+                    extras.getString(MainActivity.NODE_14_BODY_KEY));
+
+            storyTrees[14] = makeEnd(15, extras.getString(MainActivity.NODE_15_TITLE_KEY),
+                    extras.getString(MainActivity.NODE_15_BODY_KEY));
+
+            storyTrees[0].setChildren(storyTrees[1], storyTrees[2]);
+            storyTrees[1].setChildren(storyTrees[3], storyTrees[4]);
+            storyTrees[2].setChildren(storyTrees[5], storyTrees[6]);
+            storyTrees[3].setChildren(storyTrees[7], storyTrees[8]);
+            storyTrees[4].setChildren(storyTrees[9], storyTrees[10]);
+            storyTrees[5].setChildren(storyTrees[11], storyTrees[12]);
+            storyTrees[6].setChildren(storyTrees[13], storyTrees[14]);
+
+            currentTree = storyTrees[0];
+            homeTree = storyTrees[0];
+
+            String choiceMsg = currentTree.getBody() + "\n>" + currentTree.getChoice1Txt() + "\n>" + currentTree.getChoice2Txt();
+            Log.i("PlayActivity", choiceMsg);
+            title.setText(currentTree.getTitle());
+            body.setText(currentTree.getBody());
+            c1.setText(currentTree.getChoice1Txt());
+            c2.setText(currentTree.getChoice2Txt());
+        }
     }
 
     public void makeChoice(View view){
